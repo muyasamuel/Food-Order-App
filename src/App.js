facaf2937452ec64725047fd75c0529215fa7252
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-10 08:37:44 +0300

    add new images and change backgroung

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { Fragment } from "react";
+
 import Header from "./components/Header";
-import Image from './components/Image'
+
 
 
 
 function App() {
   return (
-    <Fragment>
-       <Header />
-       <Image />
-    </Fragment>
+    <div >
+        <Header />
+    </div>
+     
+    
    
   );
 }
