2051db13295216c851956b52e3225d6d75e9962d
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-15 12:39:29 +0300

    Creating a provider for the cart context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CartContext from './cart-context';
+
+
+const CartProvider = (props) => {
+
+    const cartContext =   {
+            items: [],
+            totalAmount: 0 ,
+            addItem: () => {},
+            removeItem: () => {}
+        };
+      
+    
+    
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+
+        </CartContext.Provider>
+    );
+}
+
+
+
+export default CartProvider
