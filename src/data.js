0a513e143c6e1431339e0ef28797ac194e587132
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-25 16:48:55 +0300

    Adding a list of the links

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const links = [
+    {
+        id: '1',
+        name: 'Breakfast',
+        
+    },
+    {
+        id: '2',
+        name: 'Lunch',
+        
+    },
+    {
+        id: '3',
+        name: 'Dinner',
+        
+    }
+]
