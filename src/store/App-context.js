a3f4a1f2d0c8b00a3b89796ec8e27f19d7a1ec27
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-29 21:52:29 +0300

    import mealsavailable to the appcontext

--- src/store/App-context.js
+++ src/store/App-context.js
@@ -2,8 +2,72 @@ import React, { createContext, useState } from "react";
 
 const AppContext = createContext();
 
+
+const meals = [
+    {
+        id: 'm1',
+        name: 'catlesi',
+        description: 'tasty yummy potata-like flesh',
+        price: 250 
+    },
+    {
+        id: 'm2',
+        name: 'Mahamri',
+        description: 'tasty yummy potata-like flesh',
+        price:  350 
+    },
+    {
+        id: 'm3',
+        name: 'Kaimati',
+        description: 'tasty yummy potata-like flesh',
+        price:  200 
+    },
+    {
+        id: 'm4',
+        name: 'Vitumbua',
+        description: 'tasty yummy potata-like flesh',
+        price:  150 
+    },
+    {
+        id: 'm4',
+        name: 'samosa',
+        description: 'tasty yummy potata-like flesh',
+        price:  150 
+    },
+    {
+        id: 'm5',
+        name: 'smokie',
+        description: 'tasty yummy potata-like flesh',
+        price:  100 
+    },
+    {
+        id: 'm6',
+        name: 'pizza',
+        description: 'tasty yummy potata-like flesh',
+        price:  600 
+    },
+    {
+        id: 'm7',
+        name: 'kebab',
+        description: 'tasty yummy potata-like flesh',
+        price:  150 
+    },
+    {
+        id: 'm8',
+        name: 'spagetti',
+        description: 'tasty yummy potata-like flesh',
+        price: 250 
+    },
+    {
+        id: 'm9',
+        name: 'Omena',
+        description: 'tasty yummy potata-like flesh',
+        price:  450 
+    },
+]
 const AppProvider = ({children}) => {
    const [isSidebarOpen, setisSidebarOpen] = useState(false);
+   const [mealsAvailable, setMealsAvailable] = useState(meals);
 
    const openSidebar = () => {
     setisSidebarOpen(true);
@@ -15,7 +79,7 @@ const AppProvider = ({children}) => {
 
 
 
-    return <AppContext.Provider value={{isSidebarOpen, openSidebar, closeSidebar}}>{children}</AppContext.Provider>
+    return <AppContext.Provider value={{isSidebarOpen, openSidebar, closeSidebar, mealsAvailable, setMealsAvailable}}>{children}</AppContext.Provider>
 }
 
 export {AppContext, AppProvider}
