216e5cfb3dad2ffb7c67f99a3ccc1df9639f8995
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-31 23:26:29 +0300

    Adding a contactsPage comonent and its css to dispage in the contact page

---
+++ src/components/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from  './ContactPage.module.css'
+
+function ContactPage() {
+  return (
+    <div className={classes.contactDiv}>
+        <h1>This is the contacts page</h1>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ContactPage from '../components/ContactPage';
 
 function Contact() {
   return (
     <div>
-        <h1>This is the contact page</h1>
+      <ContactPage />
     </div>
   )
 }
