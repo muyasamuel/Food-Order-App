53c0697261ec547d92de8abe3efca1b599c2c329
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-02 20:40:53 +0300

    styling the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,33 @@
-import React, { useContext } from 'react';
-import classes from './SideBar.module.css';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import classes from "./SideBar.module.css";
 import { FaTimes } from "react-icons/fa";
-import { AppContext } from '../store/App-context';
+import { AppContext } from "../store/App-context";
 
 function SideBar() {
-  const { closeSidebar }= useContext(AppContext)
+  const { closeSidebar } = useContext(AppContext);
   return (
     <div className={classes.sidebar}>
-        <h1>ReactMeals</h1>
-        <FaTimes className={classes.icon} onClick={closeSidebar}  />
+      <h1>MnetiMeals</h1>
+      <FaTimes className={classes.icon} onClick={closeSidebar} />
 
-        
-
+      <div className={classes.listDiv} >
+        <ul className={classes.list} >
+          <li className={classes.listItems}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/about" className={classes.link}>
+              About
+            </Link>
+            <Link to="/contact" className={classes.link}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SideBar;
