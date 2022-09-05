68dc3987b24b1a29e5dc85d8c5ed0121f5df4039
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-05 22:25:06 +0300

    Adding an error page for nn-existing route page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Order from './pages/Order'
+import Error from './pages/Error'
 
 
 import { Routes, Route } from "react-router-dom"
@@ -18,6 +19,7 @@ function App() {
         <Route path="about" element={ <About/> } />
         <Route path="contact" element={ <Contact/> } />
         <Route path="order" element={ <Order /> } />
+        <Route path="*" element={ <Error /> } />
       </Routes>
      
      
---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ErrorPage from '../components/ErrorPage'
+
+function Error() {
+  return (
+    <div>
+        <ErrorPage />
+    </div>
+  )
+}
+
+export default Error
